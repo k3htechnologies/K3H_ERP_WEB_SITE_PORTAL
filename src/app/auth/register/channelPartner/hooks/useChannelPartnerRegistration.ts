d6d94746be9d8616abc2e23bd4c2f9c4849266a1
Adesh17 <adesh.hrishabraj@gmail.com>
2026-09-23 12:53:37 +0530

    removed hard coded contact info

--- src/app/auth/register/channelPartner/hooks/useChannelPartnerRegistration.ts
+++ src/app/auth/register/channelPartner/hooks/useChannelPartnerRegistration.ts
@@ -12,7 +12,7 @@ const initialState = {
   Uniquekey: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
   Name: "",
   MobileNumberCountryCode: "+91",
-  MobileNumber: "9859859565",
+  MobileNumber: "",
   CompanyName: "",
   FirmsType: "",
   Designation: "",
